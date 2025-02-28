5fa0df64c93db7c5e0d160e255fab5fab7bcc6d6
gonmace <magoreal38@gmail.com>
2025-02-27 20:56:24 -0400

    final

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -1,44 +1,59 @@
+/**
+ * This is a minimal config.
+ *
+ * If you need the full config, get it from here:
+ * https://unpkg.com/browse/tailwindcss@latest/stubs/defaultConfig.stub.js
+ */
+
 module.exports = {
     content: [
-        '../../templates/*.html',
-        '../../templates/**/*.html', 
-        '../../main/src/index-main.js',
-        '../../**/templates/*.html',  
-        '!../../**/node_modules',
+        /**
+         * HTML. Paths to Django template files that will contain Tailwind CSS classes.
+         */
+
+        /*  Templates within theme app (<tailwind_app_name>/templates), e.g. base.html. */
+        '../templates/**/*.html',
+
+        /*
+         * Main templates directory of the project (BASE_DIR/templates).
+         * Adjust the following line to match your project structure.
+         */
+        '../../templates/**/*.html',
+
+        /*
+         * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
+         * Adjust the following line to match your project structure.
+         */
+        '../../**/templates/**/*.html',
+
+        /**
+         * JS: If you use Tailwind CSS in JavaScript, uncomment the following lines and make sure
+         * patterns match your project structure.
+         */
+        /* JS 1: Ignore any JavaScript in node_modules folder. */
+        // '!../../**/node_modules',
+        /* JS 2: Process all JavaScript files in the project. */
+        // '../../**/*.js',
+
+        /**
+         * Python: If you use Tailwind CSS classes in Python, uncomment the following line
+         * and make sure the pattern below matches your project structure.
+         */
+        // '../../**/*.py'
     ],
+    theme: {
+        extend: {},
+    },
     plugins: [
-        require('daisyui'),
+        /**
+         * '@tailwindcss/forms' is the forms plugin that provides a minimal styling
+         * for forms. If you don't like it or have own styling for forms,
+         * comment the line below to disable '@tailwindcss/forms'.
+         */
         require('@tailwindcss/forms'),
         require('@tailwindcss/typography'),
         require('@tailwindcss/aspect-ratio'),
+        require('daisyui'),
     ],
-    daisyui: {
-        themes: [
-          {
-            mytheme: {
-              // http://colormind.io/bootstrap/
-              // https://coolors.co/1a281f-aaaaaa-cf5535-fec601-f6f8ff
-              mytheme: {
-                "primary": "#cf5535",  // Main brand color
-                "secondary": "#fec601", // Light accent
-                "accent": "#aaaaaa", // Dark accent
-                "neutral": "#1a281f", // Dark shades
-                "base-100": "#f6f8ff", // Light shades (background)
-                "info": "#1E429F",
-                "success": "#0E9F6E",
-                "warning": "#C27803",
-                "error": "#E02424"
-              },
-            },
-          },
-          "dark",
-        ],
-      },
-      theme: {
-        extend: {
-          fontFamily: {
-            'sans': ['nunito', 'sans-serif'],
-          },
-        },
-      },
+    
 }
